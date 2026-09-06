8ff90798690fe9e336b1b46ba80b309f1b34a14f
GarmyderDmytro <dimagarmider28@gmail.com>
2026-09-06 11:36:25 +0300

    Add files via upload

---
+++ laba1.py
@@ -0,0 +1,51 @@
+print('hello world')
+
+
+#Змінні
+
+#int
+number = 92
+#float
+floatnumber = 12.2
+#string
+name = 'Oleg'
+#bool
+verification = True
+#list
+grade = [2, 4, 7, 11]
+#tuple
+birthday = (12, 5, 2009)
+#dict
+information = {'name': 'Oleg', 'age': 14}
+#set
+uniquenumber = {1, 11, 5, 2, 9}
+
+print(number)
+print(type(number))
+print(floatnumber)
+print(type(floatnumber))
+print(name)
+print(type(name))
+print(verification)
+print(type(verification))
+print(grade)
+print(type(grade))
+print(birthday)
+print(type(birthday))
+print(information)
+print(type(information))
+print(uniquenumber)
+print(type(uniquenumber))
+
+
+#Оператори
+num1 = 4
+num2 = 10
+
+print("додавання:", num1 + num2)
+print("віднімання:", num1 - num2)
+print("множення:", num1 * num2)
+print("ділення:", num1 / num2)
+print("цілочисельне ділення:", num1 // num2)
+print("остача від ділення:", num1 % num2)
+print("піднесення до степеня:", num1 ** num2)
